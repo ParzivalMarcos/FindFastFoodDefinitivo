38c707a76f98f7e1f09b104d37fd01c2695eb992
Pjunior <lapsobeta@gmail.com>
2019-10-21 17:30:12 -0300

    Feitas as telas (pesquisa e comida Japonesa) + Os vinculos de onclick para tais.

--- app/src/main/java/com/example/findfastfood/TelaPrincipal.java
+++ app/src/main/java/com/example/findfastfood/TelaPrincipal.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,5 +15,17 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+    }
+
+    public void japonesa (View view){
+        Intent it = new Intent(this, Japonesa.class);
+        startActivity(it);
+
+    }
+
+    public void pesquisa (View view){
+        Intent it = new Intent(this, Pesquisa.class);
+        startActivity(it);
+
     }
 }
