6712197b2e2c0451bdf26d8d466eae6b2c4e289d
Pjunior <lapsobeta@gmail.com>
2019-10-21 23:17:43 -0300

    Feitas as telas (pesquisa e comida Japonesa) + Os vinculos de onclick para tais.

---
+++ app/src/main/java/com/example/findfastfood/Japonesa.java
@@ -0,0 +1,16 @@
+package com.example.findfastfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Japonesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_japonesa);
+    }
+}
+
+
